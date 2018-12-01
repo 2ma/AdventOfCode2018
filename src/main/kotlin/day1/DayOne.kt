c04a5d9c479ce8091c09458b7ee04692f61db0dd
2ma <2ma.android@gmail.com>
2018-12-01 20:51:08 +0100

    adds: Day 1

---
+++ src/main/kotlin/day1/DayOne.kt
@@ -0,0 +1,27 @@
+package day1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val list = File("day1inp.txt").readLines().map { it.toInt() }
+    part1(list)
+    part2(list)
+}
+
+fun part1(list: List<Int>) {
+    println(list.sum())
+}
+
+fun part2(list: List<Int>) {
+    val freq = mutableSetOf(0)
+    var prev = 0
+    while (true) {
+        list.forEach {
+            prev += it
+            if (!freq.add(prev)) {
+                println(prev)
+                return
+            }
+        }
+    }
+}
